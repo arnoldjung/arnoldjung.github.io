090812a118dc5b1e4a356fe38319dd5ef3eb74f4
Arnold Jung <arnold.jung@gmail.com>
2022-04-23 18:27:07 -0400

    test commit from GitHubDesktop

--- keyboardchord.js
+++ keyboardchord.js
@@ -1,12 +1,12 @@
 let keydownKeys = new Set()
 let keyupKeys = new Set()
 let comboMap = new Map()
+
 function handleKeydown(e) {
     keydownKeys.add(e.key)
-    if(e.key == 'q') {
+    if (e.key == 'q') {
         resetKeys()
-    }
-    else {
+    } else {
         keydownKeys.add(e.key)
     }
 }
@@ -15,35 +15,36 @@ function resetKeys() {
     keydownKeys = new Set();
     keyupKeys = new Set()
 }
+
 function handleKeyup(e) {
-    if(e.key == 'q') {
+    if (e.key == 'q') {
         resetKeys()
-    }
-    else {
+    } else {
         keyupKeys.add(e.key)
-        if(keydownKeys.size == keyupKeys.size) {
-            console.log(getWord(keyupKeys)) 
+        if (keydownKeys.size == keyupKeys.size) {
+            console.log(getWord(keyupKeys))
             resetKeys()
         }
     }
 }
-    function sortString(text) {
-        return text.split('').sort().join('');
-    };
+
+function sortString(text) {
+    return text.split('').sort().join('');
+};
+
 function getWord(chord) {
     console.log("lookup: " + chord)
-  let key = sortString( Array.from(chord).join(""))
-  console.log("key: " + key)
-  let word
-  if(comboMap.has(key)) {
-      word = comboMap.get(key)
-      console.log(word)
-  }
-    else {
+    let key = sortString(Array.from(chord).join(""))
+    console.log("key: " + key)
+    let word
+    if (comboMap.has(key)) {
+        word = comboMap.get(key)
+        console.log(word)
+    } else {
         word = "NOT FOUND(" + key + ")";
     }
-      let wordNode = document.createTextNode("\n"+word)
-   document.getElementById("output").appendChild(wordNode)  
+    let wordNode = document.createTextNode("\n" + word)
+    document.getElementById("output").appendChild(wordNode)
 }
 
 async function loadDictionary() {
@@ -53,18 +54,17 @@ async function loadDictionary() {
         comboMap.set(sorted, word)
     }
     await fetch("corncob_lowercase.txt")
-    .then(async response => {
-        console.log(response)
-        let text = await response.text()
-        for(let line of text.split("\n")){
-            addWord(line.trim())    
-        }
-        console.log("done load")
-        
-    }
-         )
+        .then(async response => {
+            console.log(response)
+            let text = await response.text()
+            for (let line of text.split("\n")) {
+                addWord(line.trim())
+            }
+            console.log("done load")
 
-    
+        })
+
+
 }
 async function addChordInput(inputId) {
     loadDictionary()
